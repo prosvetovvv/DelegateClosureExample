95ee7ce2bdc8c9e9069353e8898f8169f17885c7
prosvetovvv <vp12@yandex.ru>
2020-12-03 13:11:26 +0300

    Initial Commit

---
+++ DelegateClosureExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateClosureExample
+//
+//  Created by Vitaly Prosvetov on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
